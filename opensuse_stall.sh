dca1f0ef0f6bf5d14d7b2d6d8fd895b91a7b5ad8
textcoffin <textcoffin@localhost.localdomain>
2026-08-23 19:21:55 +0500

    script better

---
+++ opensuse_stall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd ~/vxwm
+git clone -b custom https://github.com/TextCoffin/vxwm-keybinds-config.git
+rm -rf config.h
+mv ~/vxwm/vxwm-keybinds-config/config.h ~/vxwm
+mv ~/vxwm/vxwm-keybinds-config/custom ~/vxwm
+rm -rf ~/vxwm/vxwm-keybinds-config
+sudo make clean install
+echo "keybinds for vxwm installed succsessful!"
+
+read -p "do you want vcompmgr? (y/n): " answer
+
+case "$answer" in
+  [yY] )
+	echo "y"
+git clone https://codeberg.org/wh1tepearl/vcompmgr.git
+cd vcompmgr/
+make
+sudo make install
+	;;
+  [nN] )
+    echo "y"
+    exit 0
+    ;;
+  * )
+    echo "invalid type"
+    ;;
+esac
