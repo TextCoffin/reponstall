73f476f80632f655fe6d3f8780e5568ffc9abb67
textcoffin <textcoffin@localhost.localdomain>
2026-08-23 19:58:01 +0500

    edit susstall, make readme uses

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 repo for install the cfg
 ```bash
 git clone https://github.com/TextCoffin/reponstall.git
+cd ~/reponstall
 ```
 ```bash
 ./hyperscript.sh
--- opensuse_stall.sh
+++ opensuse_stall.sh
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 #!/bin/bash
 
-=======
->>>>>>> 743e049db8afd5e2234dbf49e80e557976be0562
 cd ~/vxwm
 git clone -b custom https://github.com/TextCoffin/vxwm-keybinds-config.git
 rm -rf config.h
@@ -10,7 +7,6 @@ mv ~/vxwm/vxwm-keybinds-config/config.h ~/vxwm
 mv ~/vxwm/vxwm-keybinds-config/custom ~/vxwm
 rm -rf ~/vxwm/vxwm-keybinds-config
 sudo make clean install
-<<<<<<< HEAD
 echo "keybinds for vxwm installed succsessful!"
 
 read -p "do you want vcompmgr? (y/n): " answer
@@ -18,13 +14,10 @@ read -p "do you want vcompmgr? (y/n): " answer
 case "$answer" in
   [yY] )
 	echo "y"
-=======
->>>>>>> 743e049db8afd5e2234dbf49e80e557976be0562
 git clone https://codeberg.org/wh1tepearl/vcompmgr.git
 cd vcompmgr/
 make
 sudo make install
-<<<<<<< HEAD
 	;;
   [nN] )
     echo "y"
@@ -34,7 +27,5 @@ sudo make install
     echo "invalid type"
     ;;
 esac
-=======
 #program for work keybinds
 sudo zypper install dmenu xclip maim playerctl kitty wmctrl xdotool imagemagick bc
->>>>>>> 743e049db8afd5e2234dbf49e80e557976be0562
